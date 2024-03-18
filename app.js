ee89d7ce519517bc435ed4b1f18ad8992019720b
Vadim J. Bychok <vadim@bychok.net>
2024-03-17 18:42:26 -0700

    Adds Conditionals

--- app.js
+++ app.js
@@ -170,3 +170,16 @@
 //     return todo.text;
 // });
 // console.log(todoCompleted);
+
+// ----------[ CONDITIONALS ]----------
+
+const x = 20;
+const y = 10;
+
+if (x > 5 || y > 10) {
+  console.log("x is 10");
+} else if (x > 10) {
+  console.log("x is greater than 10");
+} else {
+  console.log("x is less than 10");
+}
