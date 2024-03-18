525238fc732d6140a22e7b9fddb9fbe922d73d08
Vadim J. Bychok <vadim@bychok.net>
2024-03-17 19:04:20 -0700

    Adds OOP

--- app.js
+++ app.js
@@ -221,3 +221,49 @@ const addNums = (num = 1, num2 = 1) => num1 + num2;
 console.log(addNumbs(5, 4));
 
 // ----------[ OOP ]----------
+
+// Constructor Function
+// function Person(firstName, lastName, dob) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.dob = new Date(dob);
+//     this.getBirthYear = function() {
+//         return this.dob.getFullYear();
+//     }
+//     this.getFullName = function() {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// Person.prototype.getBirthYear = function() {
+//     return this.dob.getFullYear();
+// }
+
+// Person.prototype.getFullName = function() {
+//     return this.dob.getFullName();
+// }
+
+// Class
+// class Person {
+//     constructor() {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.dob = new Date(dob);
+//     }
+//     getBirthYear() {
+//         return this.dob.getFullYear();
+//     }
+
+//     getFullName() {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// // Instantiate Object
+// const person1 = new Person("John", "Doe", "4-3-1980");
+// const person2 = new Person("Mary", "Smith", "3-6-1970");
+
+// console.log(person1);
+// console.log(person2.dob.getFullYear());
+// console.log(person1.getBirthYear());
+// console.log(person1.getFullYear());
